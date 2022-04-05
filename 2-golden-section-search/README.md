70ba7f6708b5ef61f84ceca9013cb6f1300bab5b
dmitrii-artuhov <artuhovdmitrii@gmail.com>
2022-04-05 22:34:33 +0300

    Add golden section search

--- 2-golden-section-search/README.md
+++ 2-golden-section-search/README.md
@@ -1,1 +1,14 @@
 # Golden Section Search
+
+Very basic algorithm. We have an [unimodal function](https://en.wikipedia.org/wiki/Unimodality), and we want to find it's minimum (by definition of unimodality it will be the only local minimum).
+
+In my code example the algorithm finds minimum of `sin(x) - 10 / x`, on segment `[8, 12]` which will print:
+
+```
+Point of minimum: 10.9115
+Value at minimum: -1.91293
+```
+
+![Function graphic build with Desmos](http://url/to/img.png)
+
+More info here (rus): [click](http://www.machinelearning.ru/wiki/index.php?title=%D0%9C%D0%B5%D1%82%D0%BE%D0%B4_%D0%B7%D0%BE%D0%BB%D0%BE%D1%82%D0%BE%D0%B3%D0%BE_%D1%81%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D1%8F._%D0%A1%D0%B8%D0%BC%D0%BC%D0%B5%D1%82%D1%80%D0%B8%D1%87%D0%BD%D1%8B%D0%B5_%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D1%8B).
--- 2-golden-section-search/main.cpp
+++ 2-golden-section-search/main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
 #include <cassert>
+#include <cmath>
+#include <algorithm>
+
+template <typename Func>
+double GoldenSectionSearch(double l, double r, Func& F, double EPS) {
+	assert(l <= r);
+	double phi = (3 + sqrt(5)) / 2;
+
+	// calculate only 1 value instead of 2.
+	bool left_value_saved = true;
+	double saved_value = F(l + (r - l) / phi);
+
+	auto calc_value = [&](bool is_left, double x) -> double {
+		if (is_left == left_value_saved) {
+			return saved_value;
+		}
+
+		return F(x);
+	};
+
+	// implementation 
+	while (r - l > EPS) {
+		double m1 = l + (r - l) / phi;
+		double m2 = r - (r - l) / phi;
+
+		double F1 = calc_value(true, m1); 
+		double F2 = calc_value(false, m2); 
+		
+		if (F1 <= F2) {
+			r = m2;
+			left_value_saved = false;
+			saved_value = F1;
+		}
+		else {
+			l = m1;
+			left_value_saved = true;
+			saved_value = F2;
+		}
+	}
+
+	return (l + r) / 2;
+}
 
 int main() {
-	std::cout << "Hello, Golden!" << std::endl;
+	// Golden Section Search (minimization)
+	auto Function = [](double x) -> double {
+		return std::sin(x) - 10 / x;
+	};
+	double EPS = 1e-12;
+	double left_bound = 8;
+	double right_bound = 12;
+
+	double result = GoldenSectionSearch(left_bound, right_bound, Function, EPS);
+	std::cout << "Point of minimum: " << result << std::endl;
+	std::cout << "Value at minimum: " << Function(result) << std::endl;
 }
