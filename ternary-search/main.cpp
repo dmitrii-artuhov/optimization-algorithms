c0f93c6e8fb9396ba3b256169c691bfd585b5ec9
dmitrii-artuhov <artuhovdmitrii@gmail.com>
2022-04-05 21:39:34 +0300

    Add implementation of basic ternary search

--- ternary-search/main.cpp
+++ ternary-search/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <cassert>
 
+template <typename Func>
+double TernarySearch(double l, double r, Func& F, double EPS) {
+	assert(l <= r);
+
+	while (r - l > EPS) {
+		double m1 = l + (r - l) / 3;
+		double m2 = r - (r - l) / 3;
+
+		if (F(m1) <= F(m2)) {
+			r = m2;
+		}
+		else {
+			l = m1;
+		}
+	}
+
+	return (l + r) / 2;
+}
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
+	// Ternary Search (minimization)
+	auto Function = [](double x) -> double {
+		return x * x + 4 * x - 2;
+	};
+	double EPS = 1e-12;
+	double left_bound = -6;
+	double right_bound = 2;
+
+	double result = TernarySearch(left_bound, right_bound, Function, EPS);
+	std::cout << "Point of minimum: " << result << std::endl;
+	std::cout << "Value at minimum: " << Function(result) << std::endl;
 }
